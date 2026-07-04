ae35f534e765d28c50e8582ceacc25678b1f63cc
Gaddanti Sai Saran <gaddantisaisaran@gmail.com>
2026-07-04 21:47:02 +0530

    chore: configure express static file serving middleware

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static('public'));
 
 const authRoutes = require('./routes/authRoutes');
 

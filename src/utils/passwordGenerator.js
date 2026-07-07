e61c47c211bff418e5d37acdfc68dbf56d98d423
Gaddanti Sai Saran <gaddantisaisaran@gmail.com>
2026-07-07 10:50:39 +0530

    refactor(auth): update passwordGenerator.js to generate only letters

--- src/utils/passwordGenerator.js
+++ src/utils/passwordGenerator.js
@@ -10,19 +10,15 @@ function generateCustomPassword(length = 12) {
   const minLength = Math.max(length, 8);
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const lower = 'abcdefghijklmnopqrstuvwxyz';
-  const numbers = '0123456789';
-  const symbols = '!@#$%^&*()_+~`|}{[]:;?><,./-=[]';
-  const allChars = upper + lower + numbers + symbols;
+  const allChars = upper + lower;
 
   let password = '';
   // Guarantee at least one character from each character set
   password += upper[Math.floor(Math.random() * upper.length)];
   password += lower[Math.floor(Math.random() * lower.length)];
-  password += numbers[Math.floor(Math.random() * numbers.length)];
-  password += symbols[Math.floor(Math.random() * symbols.length)];
 
   // Fill up the rest of the password length with random characters
-  for (let i = 4; i < minLength; i++) {
+  for (let i = 2; i < minLength; i++) {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
 

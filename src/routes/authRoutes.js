d042fc3a7ba9bf9a19124491bb9bbdac3aa072c9
Gaddanti Sai Saran <gaddantisaisaran@gmail.com>
2026-07-08 16:57:18 +0530

    feat(i18n): define PUT /language route in authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getLoginHistory, logout, verifyLoginOTP, resendLoginOTP, forgotPassword } = require('../controllers/authController');
+const { register, login, getLoginHistory, logout, verifyLoginOTP, resendLoginOTP, forgotPassword, updateLanguage } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.post('/logout', logout);
 router.post('/verify-otp', verifyLoginOTP);
 router.post('/resend-otp', resendLoginOTP);
 router.post('/forgot-password', forgotPassword);
+router.put('/language', protect, updateLanguage);
 
 module.exports = router;

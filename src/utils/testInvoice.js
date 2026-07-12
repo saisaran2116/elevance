8e43657b0e530a418426ccb9a05cb8c4c189cc3b
Gaddanti Sai Saran <gaddantisaisaran@gmail.com>
2026-07-12 15:40:12 +0530

    test: add script to test invoice generation

---
+++ src/utils/testInvoice.js
@@ -0,0 +1,18 @@
+const invoiceService = require('./invoiceService');
+const emailService = require('./emailService');
+const fs = require('fs');
+
+async function run() {
+  const user = { name: 'Test User', email: 'test@example.com' };
+  const plan = { name: 'Gold', amount: 100000 };
+  
+  console.log('Generating invoice...');
+  const pdfBuffer = await invoiceService.generateInvoice(user, plan, 'txn_123456');
+  
+  console.log('Saving to disk...');
+  fs.writeFileSync('test_invoice.pdf', pdfBuffer);
+  
+  console.log('Done');
+}
+
+run();
